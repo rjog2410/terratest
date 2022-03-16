e3349b31a3f177a1ae26db4439cd7570eed82a3e
Rafael Ortiz <rjog@live.com.mx>
2022-03-16 12:45:45 -0600

    Ejercicio 4 - Cloud

--- firstServer/instance.tf
+++ firstServer/instance.tf
@@ -1,11 +1,13 @@
 provider "aws" {
     region = "us-east-2"
+    access_key=AWS_ACCESS_KEY_ID
+    secret_key=AWS_SECRET_ACCESS_KEY_ID
 }
 
-resource "aws_instance" "hello-instance" {
+resource "aws_instance" "instance-terraformcloud" {
     ami = "ami-0fb653ca2d3203ac1"
     instance_type = "t2.micro"
     tags = {
-        Name = "hello-instance"
+        Name = "terraformcloud"
     }
 }
