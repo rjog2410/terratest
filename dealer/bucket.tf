67807009641396c848acca3008a492b370e81659
Rafael Ortiz <rjog@live.com.mx>
2022-03-16 14:01:51 -0600

    Ejercicio 5 - bucket

--- dealer/bucket.tf
+++ dealer/bucket.tf
@@ -2,26 +2,27 @@ variable "AWS_ACCESS_KEY_ID" {
   type = string
 }
 variable "AWS_SECRET_ACCESS_KEY" {
-  type        = string
+  type = string
 }
 
 variable "bucket" {
-  type  = string
-  default = "Bucket S3 RJOG"
+  type    = string
+  default = "buckets3rjog"
 }
 
 variable "content" {
-  type  = string
+  type    = string
   default = "Class terraform RJOG"
 }
 
 variable "region" {
-  type  = string
+  type    = string
   default = "us-east-2"
 }
 
 provider "aws" {
   region     = var.region
+  profile    = "test_profile"
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
 }
@@ -36,6 +37,7 @@ resource "aws_s3_bucket" "bucket1" {
 resource "aws_s3_bucket_object" "object1" {
   bucket       = aws_s3_bucket.bucket1.bucket
   key          = "index.html"
+  acl          = "public-read"
   content      = var.content
   content_type = "text/html"
 }
