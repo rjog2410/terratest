f54349e8901342cfd326b5514b1175cf60cdae5d
Rafael Ortiz <rjog@live.com.mx>
2022-03-16 12:52:22 -0600

    Ejercicio 4 - Cloud

--- firstServer/instance.tf
+++ firstServer/instance.tf
@@ -1,13 +1,16 @@
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_ACCESS_KEY_ID" {}
+
 provider "aws" {
-    region = "us-east-2"
-    access_key=AWS_ACCESS_KEY_ID
-    secret_key=AWS_SECRET_ACCESS_KEY_ID
+  region     = "us-east-2"
+  access_key = AWS_ACCESS_KEY_ID
+  secret_key = AWS_SECRET_ACCESS_KEY_ID
 }
 
 resource "aws_instance" "instance-terraformcloud" {
-    ami = "ami-0fb653ca2d3203ac1"
-    instance_type = "t2.micro"
-    tags = {
-        Name = "terraformcloud"
-    }
+  ami           = "ami-0fb653ca2d3203ac1"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "terraformcloud"
+  }
 }
